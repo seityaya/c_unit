3eb8482aaebec052d5e56728c5977ee571f33f37
yaya <seityaya@ukr.net>
2022-12-29 20:06:56 +0300

    Init

---
+++ lib/yaya_void.h
@@ -0,0 +1,13 @@
+//Author                 : Seityagiya Terlekchi
+//Contacts               : seityaya@ukr.net
+//Creation Date          : 2022.12
+//License Link           : https://spdx.org/licenses/LGPL-2.1-or-later.html
+//SPDX-License-Identifier: LGPL-2.1-or-later
+//Copyright © 2022-2022 Seityagiya Terlekchi. All rights reserved.
+
+#ifndef YAYA_VOID_H
+#define YAYA_VOID_H
+
+
+
+#endif /*YAYA_VOID_H*/
